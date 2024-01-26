e8ffd36f58b1cd2955d4ed42192a279fed6ca583
Darpan Ashok Sanghani <darpansanghani57186@gmail.com>
2024-01-26 12:43:01 +0530

    Second change

---
+++ for loop.py
@@ -0,0 +1,15 @@
+names=['darpan','shubha','naval']
+for i in names:
+    print(i);
+
+
+for i in range(0,6):
+    print(i)
+
+
+names=['darpan','shubha','naval']
+for i in names:
+    print(i);
+    for char in i:
+        print(char)
+
